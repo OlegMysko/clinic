be815c6424e75c9bb2804d4e2e23256ec7103e11
Oleh Mysko <olegmysko1996@gmail.com>
2026-07-01 19:31:06 +0300

    add protect routes and avtologin

--- src/app/router/Root.tsx
+++ src/app/router/Root.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { App } from '../App';
 import { LoginPage } from '../../pages/LoginPage/LoginPage';
 import { AdminPage } from '@/pages/AdminPage/AdminPage';
@@ -9,6 +9,7 @@ export const Root: React.FC = () => {
   return (
     <HashRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path='/login' element={<LoginPage />} />
        <Route
   path="/admin"
@@ -18,9 +19,7 @@ export const Root: React.FC = () => {
     </ProtectedRoute>
   }
 />
-        <Route path="/" element={<App />}>
-          
-        </Route>
+        
       </Routes>
     </HashRouter>
   );
