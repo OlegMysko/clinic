ebbe6692dd0ce68b8e4a5377071df89b8b00cde3
Oleh Mysko <olegmysko1996@gmail.com>
2026-07-06 14:40:13 +0300

    add hover to nav bar

--- src/shared/config/navigation.tsx
+++ src/shared/config/navigation.tsx
@@ -8,34 +8,32 @@ import { TbCalendarTime } from "react-icons/tb";
 export const navigation = [
   {
     title: "Dashboard",
-    patch: ROUTES.DASHBOARD,
-    icon:<CiHome/>
-
+    path: ROUTES.DASHBOARD,
+    icon: <CiHome />,
   },
   {
-    title:"Reminder",
-      patch:ROUTES.REMINDER,
-    icon:<PiChats/>
+    title: "Reminder",
+    path: ROUTES.REMINDER,
+    icon: <PiChats />,
   },
-   {
-    title:"Patients",
-      patch:ROUTES.PATIENT,
-    icon:<CiUser />
+  {
+    title: "Patients",
+    path: ROUTES.PATIENT,
+    icon: <CiUser />,
   },
-    {
-    title:"Doctors",
-      patch:ROUTES.DOCTORS,
-    icon:<CiStethoscope/>,
+  {
+    title: "Doctors",
+    path: ROUTES.DOCTORS,
+    icon: <CiStethoscope />,
   },
-     {
-    title:"Appointments",
-      patch:ROUTES.APPOINTMENTS,
-    icon:<LuDock/>,
+  {
+    title: "Appointments",
+    path: ROUTES.APPOINTMENTS,
+    icon: <LuDock />,
   },
-      {
-    title:"Calendar",
-      patch:ROUTES.CALENDAR,  
-    icon:<TbCalendarTime/>
-  } 
-    
-]
+  {
+    title: "Calendar",
+    path: ROUTES.CALENDAR,
+    icon: <TbCalendarTime />,
+  },
+];
